8888b281570452e2ebc47230481337d2bbc7ecb5
Saeid Madhilaleh <saeedmadhitabriz@gmail.com>
2026-02-25 22:58:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# ==========================================
-# 🌟 فیلتر جادویی یکسان‌سازی متن 🌟
-# ==========================================
+
 def normalize(text):
     text = text.lower()
     replacements = {
@@ -26,12 +24,10 @@ def normalize(text):
         text = text.replace(tr_char, en_char)
     return text
 
-# ==========================================
-# دیتابیس‌های جدید و هوشمند
-# ==========================================
+
 vocab_dict = {}
 bigram_model = defaultdict(Counter)
-concat_dict = {} # 🌟 دیکشنری برای کلماتی که به هم چسبیدن (مثل iyimisin)
+concat_dict = {}
 
 def train_model(file_path):
     if not os.path.exists(file_path):
@@ -42,22 +38,18 @@ def train_model(file_path):
             if not words: continue
             
             for i in range(len(words)):
-                # یادگیری کلمات تکی
                 norm_w = normalize(words[i])
                 if norm_w not in vocab_dict:
                     vocab_dict[norm_w] = words[i]
                 
-                # یادگیری کلمات بعدی و کلمات چسبیده
                 if i < len(words) - 1:
                     norm_current = normalize(words[i])
                     original_next_word = words[i + 1]
                     bigram_model[norm_current][original_next_word] += 1
                     
-                    # 🌟 جادوی حل مشکل فاصله: ذخیره دو کلمه متوالی به صورت چسبیده
                     norm_next = normalize(words[i+1])
                     concat_norm = norm_current + norm_next
                     
-                    # اگر تو متن بوده "iyi misin"، تو ذهن پایتون میشه "iyimisin"
                     if concat_norm not in concat_dict:
                         concat_dict[concat_norm] = f"{words[i]} {words[i+1]}"
 
@@ -67,9 +59,7 @@ train_model("en.txt")
 train_model("tr.txt")
 print("آموزش کامل شد! آماده کار.")
 
-# ==========================================
-# API ارتباطی
-# ==========================================
+
 @app.get("/suggest")
 def get_suggestions(prefix: str = "", previous_word: str = ""):
     suggestions = []
@@ -77,16 +67,12 @@ def get_suggestions(prefix: str = "", previous_word: str = ""):
     if prefix:
         norm_prefix = normalize(prefix)
         
-        # ۱. جستجو در کلمات تکی (مثل merhaba)
         matches_single = [orig for norm, orig in vocab_dict.items() if norm.startswith(norm_prefix)]
         
-        # ۲. جستجو در کلمات دوگانه چسبیده (مثل iyimisin که پیشنهاد میده iyi misin)
         matches_concat = [combo for concat_norm, combo in concat_dict.items() if concat_norm.startswith(norm_prefix)]
         
-        # ترکیب هر دو لیست و حذف تکراری‌ها
         all_matches = list(dict.fromkeys(matches_single + matches_concat))
         
-        # مرتب‌سازی بر اساس طول و برگرداندن ۳ تای اول
         suggestions = sorted(all_matches, key=len)[:3]
         
     elif previous_word:
